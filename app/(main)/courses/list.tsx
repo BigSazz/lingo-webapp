a96d21d75d91ee454076c50e6720a3fffc0f3930
Osaze Oviawe <ocubed88@gmail.com>
2024-03-20 15:32:01 +0100

    - server actions

--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -1,14 +1,35 @@
 "use client";
 
-import { courses } from "@/db/schema";
+import { toast } from "sonner";
+import { courses, userProgress } from "@/db/schema";
 import { Card } from "./card";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { upsertUserProgress } from "@/actions/user-progress";
 
 type Props = {
   courses: (typeof courses.$inferSelect)[];
-  activeCourseId: number;
+  activeCourseId?: typeof userProgress.$inferSelect.activeCourseId;
 };
 
 export const List = ({ courses, activeCourseId }: Props) => {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
+  const onClick = (id: number) => {
+    if (isPending) return;
+
+    if (id === activeCourseId) {
+      return router.push("/learn");
+    }
+
+    startTransition(() => {
+      upsertUserProgress(id).catch(() =>
+        toast.error("Something went wrong, try again later!"),
+      );
+    });
+  };
+
   return (
     <>
       <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4">
@@ -18,8 +39,8 @@ export const List = ({ courses, activeCourseId }: Props) => {
             id={course.id}
             title={course.title}
             imageSrc={course.imageSrc}
-            onClick={() => {}}
-            disabled={false}
+            onClick={onClick}
+            disabled={isPending}
             active={course.id === activeCourseId}
           />
         ))}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,12 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 
 const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Duolingo | Web-app",
+  title: "Duolingo | Web-App",
   description: "Start learning a new language easily today",
 };
 
@@ -18,7 +19,13 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={font.className}>{children}</body>
+        <head>
+          <link rel="icon" type="images/x-icon" href="/mascot.svg" />
+        </head>
+        <body className={font.className}>
+          <Toaster />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
